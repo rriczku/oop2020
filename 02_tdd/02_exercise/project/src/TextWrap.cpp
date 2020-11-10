2609d0732d687665c4f7ce6989fbff81f98e041e
rriczku <kuba8rychlicki@gmail.com>
2020-11-10 11:11:43 +0100

    [green] four characters in one column

--- 02_tdd/02_exercise/project/src/TextWrap.cpp
+++ 02_tdd/02_exercise/project/src/TextWrap.cpp
@@ -5,9 +5,15 @@ TextWrap::TextWrap(int columns) : columns(columns) {
 }
 std::string TextWrap::wrap(const std::string& line) const
 {
+    std::string hLine;
     if(line.size()>1)
     {
-        return line.substr(0,columns)+'\n'+line.substr(columns);
+        for(int i=0;i<line.size();i+=columns)
+        {
+           hLine+=(line.substr(i,columns)+"\n");
+        }
+            hLine.pop_back();
+        return hLine;
     }
     return line;
 }
