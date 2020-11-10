d323e596227c3d3df5067412b211fd5a770f57ae
rriczku <kuba8rychlicki@gmail.com>
2020-11-10 10:44:27 +0100

    [red] four characters in one column

--- 02_tdd/02_exercise/project/tests/src/TextWrapTest.cpp
+++ 02_tdd/02_exercise/project/tests/src/TextWrapTest.cpp
@@ -30,3 +30,8 @@ TEST(TextWrap,Wrap_FourCharacterInTwoColumn)
     TextWrap textWrap{2};
     EXPECT_EQ("ab\ncd",textWrap.wrap("abcd"));
 }
+TEST(TextWrap,Wrap_FourCharacterInFourColumn)
+{
+    TextWrap textWrap{1};
+    EXPECT_EQ("a\nb\nc\nd",textWrap.wrap("abcd"));
+}
