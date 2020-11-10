f7f19443a007ed992e9f391a230c44f65ea11d75
rriczku <kuba8rychlicki@gmail.com>
2020-11-10 10:03:21 +0100

    [red] wrap one character in one column

--- 02_tdd/02_exercise/project/tests/src/TextWrapTest.cpp
+++ 02_tdd/02_exercise/project/tests/src/TextWrapTest.cpp
@@ -10,3 +10,8 @@ TEST(TextWrap, Constructor_PassNumberOfColumnsThroughTheConstructor)
 {
     TextWrap textWrap{2};
 }
+TEST(TextWrap,Wrap_OneCharacterInOneColumn)
+{
+    TextWrap textWrap{1};
+    EXPECT_EQ("a",textWrap("a"));
+}
