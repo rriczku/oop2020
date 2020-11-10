883b9c62239957552e6af2b121d4b34372dd49c3
rriczku <kuba8rychlicki@gmail.com>
2020-11-10 10:22:40 +0100

    [green] four characters in two columns

--- 02_tdd/02_exercise/project/src/TextWrap.cpp
+++ 02_tdd/02_exercise/project/src/TextWrap.cpp
@@ -7,7 +7,7 @@ std::string TextWrap::wrap(const std::string& line) const
 {
     if(line.size()>1)
     {
-        return line.substr(0,1)+'\n'+line.substr(1);
+        return line.substr(0,columns)+'\n'+line.substr(columns);
     }
     return line;
 }
