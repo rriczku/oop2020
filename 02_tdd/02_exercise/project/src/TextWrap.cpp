ba8f1723ffc6409e9f6f180472d501f1bc73a40b
rriczku <kuba8rychlicki@gmail.com>
2020-11-10 10:17:59 +0100

    [green] two characters in one column

--- 02_tdd/02_exercise/project/src/TextWrap.cpp
+++ 02_tdd/02_exercise/project/src/TextWrap.cpp
@@ -5,5 +5,9 @@ TextWrap::TextWrap(int) {
 }
 std::string TextWrap::wrap(const std::string& line) const
 {
+    if(line.size()>1)
+    {
+        return line.substr(0,1)+'\n'+line.substr(1);
+    }
     return line;
 }
