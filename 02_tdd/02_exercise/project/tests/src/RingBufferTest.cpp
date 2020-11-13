758f8d871d9b87828af106a04b917c54a4644909
rriczku <kuba8rychlicki@gmail.com>
2020-11-13 11:19:54 +0100

    [red] add six elements to buffer

--- 02_tdd/02_exercise/project/tests/src/RingBufferTest.cpp
+++ 02_tdd/02_exercise/project/tests/src/RingBufferTest.cpp
@@ -38,12 +38,11 @@ TEST(RingBuffer, DeleteElementElementFromBuffer)
     ringBuffer.add(4);
     EXPECT_EQ(3,ringBuffer.remove());
 }
-/*TEST(RingBuffer, AddSixElementsToBuffer)
+TEST(RingBuffer, AddSixElementsToBuffer)
 {
     RingBuffer ringBuffer{5};
     int* tab = new int[5];
     tab[0]=6;tab[1]=2;tab[2]=3;tab[3]=4;tab[4]=5;
     ringBuffer.add(1);ringBuffer.add(2);ringBuffer.add(3);ringBuffer.add(4);ringBuffer.add(5);ringBuffer.add(6);
     EXPECT_TRUE(0 == std::memcmp(tab,ringBuffer.array,sizeof(tab)));
-}*/
-// TODO: ...
+}
