cecb17ea0060fe66f7d7f3802810a729708771ca
rriczku <kuba8rychlicki@gmail.com>
2020-11-13 13:16:41 +0100

    [red] Remove and add element to buffer5

--- 02_tdd/02_exercise/project/tests/src/RingBufferTest.cpp
+++ 02_tdd/02_exercise/project/tests/src/RingBufferTest.cpp
@@ -59,3 +59,72 @@ TEST(RingBuffer, RemoveAndAddElementToBuffer)
 
     EXPECT_TRUE(0 == std::memcmp(tab,ringBuffer.array,5*sizeof(int)));
 }
+
+TEST(RingBuffer, RemoveAndAddElementToBuffer2)
+{
+    RingBuffer ringBuffer{5};
+    int* tab = new int[5];
+    tab[0]=0;tab[1]=2;tab[2]=3;tab[3]=4;tab[4]=5;
+    ringBuffer.add(1);
+    ringBuffer.add(2);
+    ringBuffer.add(3);
+    ringBuffer.remove();
+    ringBuffer.add(4);
+    ringBuffer.add(5);
+
+    EXPECT_TRUE(0 == std::memcmp(tab,ringBuffer.array,5*sizeof(int)));
+}
+
+TEST(RingBuffer, RemoveAndAddElementToBuffer3)
+{
+    RingBuffer ringBuffer{5};
+    int* tab = new int[5];
+    tab[0]=6;tab[1]=2;tab[2]=3;tab[3]=4;tab[4]=5;
+    ringBuffer.add(1);
+    ringBuffer.add(2);
+    ringBuffer.add(3);
+    ringBuffer.remove();
+    ringBuffer.add(4);
+    ringBuffer.add(5);
+    ringBuffer.add(6);
+
+    EXPECT_TRUE(0 == std::memcmp(tab,ringBuffer.array,5*sizeof(int)));
+}
+TEST(RingBuffer, RemoveAndAddElementToBuffer4)
+{
+    RingBuffer ringBuffer{5};
+    int* tab = new int[5];
+    tab[0]=6;tab[1]=7;tab[2]=3;tab[3]=4;tab[4]=5;
+    ringBuffer.add(1);
+    ringBuffer.add(2);
+    ringBuffer.add(3);
+    ringBuffer.remove();
+    ringBuffer.add(4);
+    ringBuffer.add(5);
+    ringBuffer.add(6);
+    ringBuffer.add(7);
+
+
+    EXPECT_TRUE(0 == std::memcmp(tab,ringBuffer.array,5*sizeof(int)));
+}
+TEST(RingBuffer, RemoveAndAddElementToBuffer5)
+{
+    RingBuffer ringBuffer{5};
+    int* tab = new int[5];
+    tab[0]=6;tab[1]=7;tab[2]=0;tab[3]=4;tab[4]=5;
+    ringBuffer.add(1);
+    ringBuffer.add(2);
+    ringBuffer.add(3);
+    ringBuffer.remove();
+    ringBuffer.add(4);
+    ringBuffer.add(5);
+    ringBuffer.add(6);
+    ringBuffer.add(7);
+    ringBuffer.remove();
+    for(int i=0;i<5;i++)
+    {
+        std::cout<<ringBuffer.array[i]<<std::endl;
+    }
+
+    EXPECT_TRUE(0 == std::memcmp(tab,ringBuffer.array,5*sizeof(int)));
+}
