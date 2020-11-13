3a08ef63aaba46bb7bb02763eb5b7eef8f9abadc
rriczku <kuba8rychlicki@gmail.com>
2020-11-13 10:58:56 +0100

    [green] add two elements to buffer

--- 02_tdd/02_exercise/project/src/RingBuffer.cpp
+++ 02_tdd/02_exercise/project/src/RingBuffer.cpp
@@ -1,11 +1,11 @@
+#include <iostream>
 #include "RingBuffer.h"
 
 RingBuffer::RingBuffer(int capacity) : capacity(capacity){
-
     array = new int [capacity];
     currentSize=0;
-
 }
 void RingBuffer::add(int value){
     array[currentSize]=value;
+    currentSize++;
 }
--- 02_tdd/02_exercise/project/tests/src/RingBufferTest.cpp
+++ 02_tdd/02_exercise/project/tests/src/RingBufferTest.cpp
@@ -13,12 +13,20 @@ TEST(TextWrap, Constructor_PassCapacityThroughTheConstructor)
 TEST(TextWrap, AddOneElementToBuffer)
 {
     RingBuffer ringBuffer{5};
-    ringBuffer.add(10);
     int* tab = new int[5];
     tab[0]=3;
     ringBuffer.add(3);
     EXPECT_TRUE(0 == std::memcmp(tab,ringBuffer.array,sizeof(tab)));
 }
-
+TEST(TextWrap, AddTwoElementToBuffer)
+{
+    RingBuffer ringBuffer{5};
+    int* tab = new int[5];
+    tab[0]=3;
+    tab[1]=4;
+    ringBuffer.add(3);
+    ringBuffer.add(4);
+    EXPECT_TRUE(0 == std::memcmp(tab,ringBuffer.array,sizeof(tab)));
+}
 
 // TODO: ...
