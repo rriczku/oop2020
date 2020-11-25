8cf0b85288ee99564fb6d14d2c1811bfd9a1b04d
rriczku <kuba8rychlicki@gmail.com>
2020-11-25 20:52:19 +0100

    SmallBench done

--- 04_stl_benchmark/project/benchmarks/src/SmallBench.cpp
+++ 04_stl_benchmark/project/benchmarks/src/SmallBench.cpp
@@ -2,3 +2,40 @@
 #include "BenchIncludes.h"
 
 // TODO: Add benchmarks for operator<. operator==, and hash
+
+static void LessThan(State& state)
+{
+    Small a{};
+    Small b{};
+    a.randomize();
+    b.randomize();
+
+    for(auto _ : state){
+        a<b;
+    }
+}
+BENCHMARK(LessThan);
+
+static void Equal(State& state)
+{
+    Small a{};
+    Small b{};
+    a.randomize();
+    b.randomize();
+
+    for(auto _ : state){
+        a==b;
+    }
+}
+BENCHMARK(Equal);
+static void Hash(State& state)
+{
+    Small a{};
+    std::hash<Small> hash{};
+    a.randomize();
+
+    for(auto _ : state){
+        hash(a);
+    }
+}
+BENCHMARK(Hash);
