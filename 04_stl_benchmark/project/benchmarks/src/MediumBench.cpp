5486570d243814f2f8402ed815cd04029e3d4fa6
rriczku <kuba8rychlicki@gmail.com>
2020-11-25 21:04:28 +0100

    MediumBench done

--- 04_stl_benchmark/project/benchmarks/src/MediumBench.cpp
+++ 04_stl_benchmark/project/benchmarks/src/MediumBench.cpp
@@ -1,4 +1,40 @@
 #include "Medium.h"
 #include "BenchIncludes.h"
 
-// TODO: Add benchmarks for operator<. operator==, and hash
+
+static void MediumLessThan(State& state)
+{
+    Medium a{};
+    Medium b{};
+    a.randomize();
+    b.randomize();
+
+    for(auto _ : state){
+        a<b;
+    }
+}
+BENCHMARK(MediumLessThan);
+
+static void MediumEqual(State& state)
+{
+    Medium a{};
+    Medium b{};
+    a.randomize();
+    b.randomize();
+
+    for(auto _ : state){
+        a==b;
+    }
+}
+BENCHMARK(MediumEqual);
+static void MediumHash(State& state)
+{
+    Medium a{};
+    std::hash<Medium> hash{};
+    a.randomize();
+
+    for(auto _ : state){
+        hash(a);
+    }
+}
+BENCHMARK(MediumHash);
