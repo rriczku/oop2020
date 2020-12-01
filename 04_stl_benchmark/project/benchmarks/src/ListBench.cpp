08b0dc4c19ffdfa4deb878b30e4b30672b16a644
rriczku <kuba8rychlicki@gmail.com>
2020-12-01 23:19:29 +0100

    Small_List benches done

--- 04_stl_benchmark/project/benchmarks/src/ListBench.cpp
+++ 04_stl_benchmark/project/benchmarks/src/ListBench.cpp
@@ -86,14 +86,370 @@ static void Small_List_Clear(State& state)
         state.ResumeTiming();
 
         list.clear();
+        DoNotOptimize(list);
         ClobberMemory();
     }
     state.SetComplexityN(state.range(0));
 }
 BENCHMARK(Small_List_Clear)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
 
+static void Small_List_Insert(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+    std::list<Small> list(size);
+    Small s{};
+    auto it=list.begin();
+    for(auto _ :state)
+    {
+        state.PauseTiming();
+        s.randomize();
+        state.ResumeTiming();
+        DoNotOptimize(list.insert(it++,s));
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Insert)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+static void Small_List_Construct(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+
+    for(auto _ :state)
+    {
+        std::list<Small> s(size);
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Construct)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+static void Small_List_Erase(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+    std::list<Small> l(size);
+    Small s{};
+    for(auto _ :state)
+    {
+      state.PauseTiming();
+      s.randomize();
+      auto it=l.begin();
+      l.insert(it,s);
+      state.ResumeTiming();
+      DoNotOptimize(l.erase(it));
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Erase)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+static void Small_List_Push_Back(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+    std::list<Small> l(size);
+    Small s{};
+    for(auto _ :state)
+    {
+        s.randomize();
+        l.push_back(s);
+        DoNotOptimize(l);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Push_Back)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
 
 
+static void Small_List_Pop_Back(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+    std::list<Small> l(size);
+    Small s{};
+    for(auto _ :state)
+    {
+        s.randomize();
+        l.push_back(s);
+        l.pop_back();
+        DoNotOptimize(l);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Pop_Back)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+static void Small_List_Push_Front(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+    std::list<Small> l(size);
+    Small s{};
+    for(auto _ :state)
+    {
+        s.randomize();
+        l.push_front(s);
+        DoNotOptimize(l);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Push_Front)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+static void Small_List_Pop_Front(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+    std::list<Small> l(size);
+    Small s{};
+    for(auto _ :state)
+    {
+        s.randomize();
+        l.push_front(s);
+        l.pop_front();
+        DoNotOptimize(l);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Pop_Front)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+static void Small_List_Resize(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+
+    for(auto _ :state)
+    {
+        state.PauseTiming();
+        int r=rand()%size;
+        std::list<Small> l(size);
+        state.ResumeTiming();
+
+        l.resize(r);
+        DoNotOptimize(l);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Resize)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+static void Small_List_Swap(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+    std::list<Small> s1(size);
+    std::list<Small> s2(size);
+    for(auto i=0;i<size;i++)
+    {
+        Small s{};
+        s.randomize();
+        s1.push_back(s);
+    }
+    for(auto i=0;i<size;i++)
+    {
+        Small s{};
+        s.randomize();
+        s2.push_back(s);
+    }
+    for(auto _ :state)
+    {
+        s1.swap(s2);
+        DoNotOptimize(s1);
+        DoNotOptimize(s2);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Swap)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+static void Small_List_Merge(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+    std::list<Small> s1(size);
+    std::list<Small> s2(size);
+    Small s{};
+    for(auto _ :state)
+    {
+        state.PauseTiming();
+        for(auto i=0;i<size;i++)
+        {
+            s.randomize();
+            s1.push_back(s);
+        }
+        for(auto i=0;i<size;i++)
+        {
+            s.randomize();
+            s2.push_back(s);
+        }
+        state.ResumeTiming();
+        s1.merge(s2);
+        DoNotOptimize(s1);
+        DoNotOptimize(s2);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Merge)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+/*
+static void Small_List_Splice(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+    std::list<Small> s1{};
+
+    Small s{};
+    for(auto _ :state)
+    {
+
+        s1.clear();
+        std::list<Small> s2{};
+        for(auto i=0;i<size;i++)
+        {
+            s.randomize();
+            s2.push_back(s);
+        }
+        auto it=s1.begin();
+
+        s1.splice(it,s2);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Splice)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+ */
+
+
+static void Small_List_Remove(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+
+    for(auto _ :state)
+    {
+        state.PauseTiming();
+        std::list<Small> l{};
+        for(auto i=0;i<size;i++)
+        {
+            Small s{};
+            s.randomize();
+            l.push_back(s);
+        }
+        Small sm{};
+        sm.randomize();
+        state.ResumeTiming();
+        l.remove(sm);
+        DoNotOptimize(l);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Remove)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+
+static void Small_List_Remove_If(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+
+    struct isLess{
+        bool operator()(const Small& s){return s.data[0]<100;}
+    };
+    for(auto _ :state)
+    {
+        state.PauseTiming();
+        std::list<Small> l{};
+        for(auto i=0;i<size;i++)
+        {
+            Small s{};
+            s.randomize();
+            l.push_back(s);
+        }
+        Small sm{};
+        sm.randomize();
+        state.ResumeTiming();
+        l.remove_if(isLess());
+        DoNotOptimize(l);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Remove_If)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+static void Small_List_Reverse(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+
+    std::list<Small> l{};
+    for(auto i=0;i<size;i++)
+    {
+        Small s{};
+        s.randomize();
+        l.push_back(s);
+    }
+    for(auto _ :state)
+    {
+        l.reverse();
+        DoNotOptimize(l);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Reverse)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+static void Small_List_Unique(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+
+    for(auto _ :state)
+    {
+        state.PauseTiming();
+        std::list<Small> l{};
+        for(auto i=0;i<size;i++)
+        {
+            Small s{};
+            s.randomize();
+            l.push_back(s);
+        }
+        state.ResumeTiming();
+
+        l.unique();
+        DoNotOptimize(l);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Unique)->RangeMultiplier(2)->Range(1u <<5u,1u<<10u)->Complexity();
+
+static void Small_List_Sort(State& state)
+{
+    auto N=state.range(0);
+    auto size=(std::size_t)N;
+
+    for(auto _ :state)
+    {
+        state.PauseTiming();
+        std::list<Small> l{};
+        for(auto i=0;i<size;i++)
+        {
+            Small s{};
+            s.randomize();
+            l.push_back(s);
+        }
+        state.ResumeTiming();
+
+        l.sort();
+        DoNotOptimize(l);
+        ClobberMemory();
+    }
+    state.SetComplexityN(state.range(0));
+}
+BENCHMARK(Small_List_Sort)->RangeMultiplier(2)->Range(1u <<5u,1u<<15u)->Complexity();
 //---------------------------------MEDIUM-------------------------------------------------
 
 static void Medium_List_Front(State& state)
